fd3a014c281335e129e804e0d0d9807db394c172
rawalanubhav <detroid.stan@gmail.com>
2018-07-18 23:21:36 +0545

    Site commitment

---
+++ ext/main.js
@@ -0,0 +1,25 @@
+$(".navbar a").click(function(){
+  $("body,html").animate({
+   scrollTop:$("#" + $(this).data('value')).offset().top
+  },1000)
+  
+ });
+
+// $(function() {
+//  $('[data-toggle="datepicker"]').datepicker({
+//    autoHide: true,
+//    zIndex: 2048,
+//  });
+// });
+$(function () {
+  $('#datetimepicker3').datetimepicker({
+      format: 'LT'
+  });
+});
+
+$(function () {
+  $('#datetimepicker4').datetimepicker({
+    format: 'L'
+  });
+});
+
